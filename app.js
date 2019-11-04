45d18c024b121285754b1f8106f6896f3b37d027
Armedi <armedi@outlook.com>
2019-11-04 09:51:57 +0700

    changed to todos example

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const EventEmitter = require('events')
 
-var jokes = require('./jokes.json');
+const jokes = require('./jokes.json');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/garing', (req, res) => {
   res.set('Content-Type', 'text/event-stream')
@@ -26,5 +28,54 @@ app.get('/garing', (req, res) => {
   sendJokeEvent()
   setInterval(sendJokeEvent, 5000)
 });
+
+const todos = [
+  {
+    id: 1,
+    todo: 'Write todo'
+  }
+]
+
+const todoEmitter = new EventEmitter();
+
+app.get('/todos', (req, res) => {
+  res.set('Content-Type', 'text/event-stream')
+  res.write(`
+    \nevent: todos
+    \ndata: ${JSON.stringify(todos)}
+    \n\n
+  `)
+  todoEmitter.on('newtodo', function (todo) {
+    res.write(`
+      \nevent: newtodo
+      \ndata: ${JSON.stringify(todo)}
+      \n\n
+    `)
+  })
+  todoEmitter.on('deltodo', function (id) {
+    res.write(`
+      \nevent: deltodo
+      \ndata: ${id}
+      \n\n
+    `)
+  })
+})
+
+app.post('/todo', (req, res) => {
+  const todo = {
+    id: todos[todos.length - 1].id + 1,
+    todo: req.body.todo
+  }
+  todos.push(todo)
+  todoEmitter.emit('newtodo', todo)
+  res.json({ success: true })
+})
+
+app.delete('/todo', (req, res) => {
+  const id = Number(req.body.id)
+  todos.splice(todos.findIndex(todo => todo.id === id), 1)
+  todoEmitter.emit('deltodo', id)
+  res.json({ success: true })
+})
 
 module.exports = app;
--- public/main.js
+++ public/main.js
@@ -1,7 +1,49 @@
-const evtSource = new EventSource("/garing");
+// const evtSource = new EventSource("/garing");
 
-evtSource.addEventListener('randomjoke', event => {
-  const joke = JSON.parse(event.data)
-  document.querySelector('#setup').innerHTML = joke.setup
-  document.querySelector('#punchline').innerHTML = joke.punchline
+// evtSource.addEventListener('randomjoke', event => {
+//   const joke = JSON.parse(event.data)
+//   document.querySelector('#setup').innerHTML = joke.setup
+//   document.querySelector('#punchline').innerHTML = joke.punchline
+// })
+
+var app = new Vue({
+  el: '#todos',
+  data: {
+    todos: [],
+    newTodo: ''
+  },
+  mounted: function () {
+    const evtSource = new EventSource("/todos");
+    evtSource.addEventListener('todos', event => {
+      const todos = JSON.parse(event.data)
+      this.todos = todos
+    })
+    evtSource.addEventListener('newtodo', event => {
+      const todo = JSON.parse(event.data)
+      this.todos.push(todo)
+    })
+    evtSource.addEventListener('deltodo', event => {
+      const id = Number(event.data)
+      this.todos.splice(this.todos.findIndex(todo => todo.id === id), 1)
+    })
+  },
+  methods: {
+    submitTodo: async function () {
+      await fetch('/todo', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          todo: this.newTodo
+        })
+      })
+      this.newTodo = ''
+    },
+    delTodo: async function (id) {
+      await fetch('/todo', {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id })
+      })
+    }
+  }
 })
